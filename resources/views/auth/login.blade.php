bc7decaa5b7f12d933fc0f993e90732ec3c9910a
ahmed saleh <ahmedsaleh@nuwas.net>
2024-01-23 23:22:14 +0200

    add-login-button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,6 +50,14 @@
                                 </div>
                             </div>
                         </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
